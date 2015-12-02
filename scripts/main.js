d78bcf52d795fe467ff5d7add412cf2836c77f50
David <djensen81@gmail.com>
2015-12-02 15:12:42 -0500

    added in routes

--- scripts/main.js
+++ scripts/main.js
@@ -5,6 +5,7 @@ var ReactRouter			 = require('react-router');
 var Router     			 = ReactRouter.Router;
 var Route      			 = ReactRouter.Route;
 var Navigation  		 = ReactRouter.Navigation;
+var History				 = ReactRouter.History;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 
 var h = require('./helpers.js');
@@ -81,11 +82,18 @@ StorePicker
  */
 
 var StorePicker = React.createClass({
-
+	mixins : [History],
+	goToStore : function(event){
+		event.preventDefault();
+		//get the data from the input
+		var storeId = this.refs.storeId.value;
+		//transition from StorePicker to App
+		this.history.pushState(null, '/store/' + storeId);
+	},
 	render : function(){
 		var name = "name";
 		return(
-			<form className="store-selector">
+			<form className="store-selector" onSubmit={this.goToStore}>
 			{/* example comment inside JSX */}
 				<h2>Please Enter a store</h2>
 				<input type="text" ref="storeId" defaultValue={h.getFunName()} required/>
