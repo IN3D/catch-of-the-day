4ae934f2a22e0b8bd072be20e9de532914df8dbd
David <djensen81@gmail.com>
2015-12-02 14:22:27 -0500

    added in funny names to the store

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,13 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+var React      			 = require('react');
+var ReactDOM    		 = require('react-dom');
 
+var ReactRouter			 = require('react-router');
+var Router     			 = ReactRouter.Router;
+var Route      			 = ReactRouter.Route;
+var Navigation  		 = ReactRouter.Navigation;
+var createBrowserHistory = require('history/lib/createBrowserHistory');
+
+var h = require('./helpers.js');
 /*
 	App
  */
@@ -34,7 +41,8 @@ var Header = React.createClass({
 					<span className="of">of</span>
 					<span className="the">the</span>
 				</span>
-				Day</h1>
+				Day
+				</h1>
 				<h3 className="tagline"><span>{this.props.tagline}</span></h3>
 			</header>
 
@@ -79,12 +87,30 @@ var StorePicker = React.createClass({
 		return(
 			<form className="store-selector">
 			{/* example comment inside JSX */}
-				<h2>Please Enter a store {name}</h2>
-				<input type="text" ref="storeId" required/>
+				<h2>Please Enter a store</h2>
+				<input type="text" ref="storeId" defaultValue={h.getFunName()} required/>
 				<input type="Submit" />
 			</form>
 		)
 	}
 });
 
-ReactDOM.render(<App/>, document.querySelector('#main'));
+var NotFound = React.createClass({
+	render : function(){
+		return <h1>Not Found!</h1>
+	}
+});
+
+/*
+	Routes
+ */
+
+var routes = (
+	<Router history={createBrowserHistory()}>
+		<Route path="/" component={StorePicker} />
+		<Route path="/store/:storeId" component={App} />
+		<Route path="*" component={NotFound} />
+	</Router>
+)
+
+ReactDOM.render(routes, document.querySelector('#main'));
