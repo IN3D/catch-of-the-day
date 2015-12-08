d885706ebc3d1eb3cfc8a1b819d16e52edcf86f7
David <djensen81@gmail.com>
2015-12-08 14:15:28 -0500

    changed over the imports to ES6 syntax, and added in animation for orders

---
+++ scripts/components/StorePicker.js
@@ -0,0 +1,32 @@
+/*
+StorePicker
+ */
+
+import React from 'react';
+import { Navigation } from 'react-router';
+import { History } from 'react-router';
+import helpers from '../helpers';
+
+var StorePicker = React.createClass({
+    mixins : [History],
+    goToStore : function(event){
+        event.preventDefault();
+        //get the data from the input
+        var storeId = this.refs.storeId.value;
+        //transition from StorePicker to App
+        this.history.pushState(null, '/store/' + storeId);
+    },
+    render : function(){
+        var name = "name";
+        return(
+            <form className="store-selector" onSubmit={this.goToStore}>
+            {/* example comment inside JSX */}
+                <h2>Please Enter a store</h2>
+                <input type="text" ref="storeId" defaultValue={helpers.getFunName()} required/>
+                <input type="Submit" />
+            </form>
+        )
+    }
+});
+
+export default StorePicker;
